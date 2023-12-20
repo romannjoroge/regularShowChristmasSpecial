aedf733420ee8f933552b868d9335216c5a071cf
romannjoroge <roman.njoroge@njuguna.com>
2023-12-20 20:34:56 +0300

    feat: creating model

---
+++ backend/ai_functions/model.js
@@ -0,0 +1,14 @@
+// This file creates the model that we will be using
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Give access to environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+// To use the service we have to first connect to it, we use our api key to do that
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+// We create a model using the connection we've made
+const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+export default model;
