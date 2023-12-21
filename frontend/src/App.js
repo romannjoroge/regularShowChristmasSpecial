c90d31063ec9cdc24c95bfe87a4ed13044584254
romannjoroge <roman.njoroge@njuguna.com>
2023-12-21 23:52:05 +0300

    feat: Completed UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import Chat from './components/chat';
+import Sidebar from './components/sidebar';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  // State for previous chats
+  const [previousChats, setPreviousChats] = useState([
+    {
+      contact: "Benson",
+      id:"benson",
+      chats: []
+    },
+    {
+      contact: "Skips",
+      id:"skips",
+      chats: []
+    },
+    {
+      contact: "Mordecai",
+      id:"mordecai",
+      chats: []
+    },
+    {
+      contact: "Rigby",
+      id:"rigby",
+      chats: []
+    },
+    {
+      contact: "Muscle Man",
+      id:"muscleman",
+      chats: []
+    },
+    {
+      contact: "High 5 Ghost",
+      id:"high5ghost",
+      chats: []
+    },
+    {
+      contact: "Aileen",
+      id:"aileen",
+      chats: []
+    },
+    {
+      contact: "Pops",
+      id:"pops",
+      chats: []
+    }
+  ]);
+  let [selectedRecieiver, setSelectedReceiver] = useState('benson')
+
+  let selectedChat = previousChats.find((e) => e.id === selectedRecieiver)
+
+  function updateMessages(message, role) {
+    let newSelectedChat = {...selectedChat}
+    newSelectedChat.chats.push({role: role, parts: message});
+    let chatIndex = previousChats.indexOf(selectedChat);
+    previousChats.splice(chatIndex, 1);
+    previousChats.push(newSelectedChat);
+    setPreviousChats(previousChats);
+  }
+
+  function changeReceiver(newReceiver) {
+    setSelectedReceiver(newReceiver);
+  }
+
+  return (<div className="app-content">
+    <Sidebar previousChats={previousChats} changeReceiver={changeReceiver}/>
+    <Chat receiver={selectedRecieiver} messages={previousChats.find((e) => e.id === selectedRecieiver).chats} updateMessages={updateMessages}/>
+  </div>);
 }
-
-export default App;
